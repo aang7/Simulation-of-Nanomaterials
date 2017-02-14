d7110a8bad1ed0c6c1f4bc8e458abd7845e5dac4
aang7 <aang.drummer@gmail.com>
2017-02-14 15:09:10 -0600

    revolucion total ahora si se mueve como deberia (yet in construction)

--- EP2/program2.py
+++ EP2/program2.py
@@ -8,24 +8,11 @@ import pygame
 from math import sqrt
 
 
-#clase 
-class Particle:
-    #"constructor"
-    def __init__(self, (x, y), size): 
-        self.x = x
-        self.y = y
-        self.size = size
-        self.colour = (random.randint(0, 255), random.randint(0, 255), 255)
-        self.thickness = 0
-
-    def display(self):
-        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
-
-
 particulas = list()
+(width, height) = (800, 700)
 
 p = 30 #cuantas divisiones tengo desde el origen hasta l
-l = 3.5 #Longitud maxima
+l = height  #Longitud maxima
 paso = l / p #Paso
 permitidos = [x * paso - l for x in range(2 * p + 1)] #Valores permitidos
 
@@ -37,69 +24,55 @@ for y in range(8): #8 particulas
         
     particulas.append(coordenadas) #Añadiendo particulas
 
-filename = 'salida.txt'
-abrir = open(filename, "w")
-
-#distancias eucladiana
-for p in particulas:
-    #print(sqrt(sum([x**2 for x in p])))
-    abrir.write(str(sqrt(sum([x**2 for x in p]))) + "\n") #Escribiendo en archivo
-
-#cmd = ['Rscript', '--vanilla','readLine.R', filename]
-
-#ahora a ejecutar R script
-#retcode = subprocess.call(cmd)
-
     
 background_colour = (255,255,255)
-(width, height) = (800, 700)
 
+#Setting up pygame screen
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption('Trying to move particles')
 screen.fill(background_colour)
 
 x = 0
 y = 0
-repeticiones = 5
 puntos_list = list()
 xHalf = width/2
 yHalf = height/2
 
+size = 5
+colour = (random.randint(0, 255), random.randint(0, 255), 255)
+thickness = 0
 
 
+clock = pygame.time.Clock()
 running = True
 switch = True
-
+cambio = 10
 while running:
-    #screen.fill(background_colour)
+    screen.fill(background_colour)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-            
-    puntos_list = []
     
-    for i in range(repeticiones):
-        size = width * (random.randint(10, 11)/1000.0)    
-        if (random.random() <= 0.5):
-            if switch:
-                x =random.randint(0 , int(width - size))
-                y =random.randint(0, int(height - size))
-                switch = False
-            else:
-                x =random.randint(0, int(width - size))
-                y =random.randint(0, int(height - size))
-                switch = True
+    for i in range(len(particulas)):
+        switch = bool(random.getrandbits(1)) #Faster than random.choice([True, False])
+        xoy = random.randint(0, 1);
+        if switch:
+            particulas[i][xoy] += cambio
+        else:
+            particulas[i][xoy] -= cambio
 
-            puntos_list.append(Particle((x , y ), int(size)))  
-    #drawing particles
-    for particle in puntos_list:
-        particle.display()
-        
-        
+        if particulas[i][xoy] < 0:
+            particulas[i][xoy] += l
+        elif particulas[i][xoy] > l:
+            particulas[i][xoy] -= l
+
+    for x in particulas:
+        pygame.draw.circle(screen, colour, tuple([int(e) for e in x]), size, thickness)
+
+    
     pygame.display.flip()
-    
+    clock.tick(12)
 
 pygame.quit()
     
